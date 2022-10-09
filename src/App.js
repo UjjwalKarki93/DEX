453e63f98330c822b5dcdfc28e8929c1d702071b
Nishan Bhandari <bnishan18@gmail.com>
2022-10-09 18:57:00 +0545

    NavBar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./components/NavBar/Navbar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div style={{ backgroundColor: " #f7f0f6", height: "100vh" }}>
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;

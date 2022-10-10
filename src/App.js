d7e588586adaf9b2a4e0dca308de4d86c8e48c2c
Nishan Bhandari <bnishan18@gmail.com>
2022-10-10 16:25:16 +0545

    Home Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Home from "./components/Home/Home";
 import Navbar from "./components/NavBar/Navbar";
 
 const App = () => {
   return (
     <div style={{ backgroundColor: " #f7f0f6", height: "100vh" }}>
       <Navbar />
+      <Home />
     </div>
   );
 };

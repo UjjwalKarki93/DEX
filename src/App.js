15ebb1346e5de3f6723f0dac5a8b7ddfa7a330c0
UjjwalKarki93 <karkiujjwal32@gmail.com>
2022-10-11 18:09:53 +0545

    changes made

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import Home from "./components/Home/Home";
 import Navbar from "./components/NavBar/Navbar";
-import { TransactionProvider } from "./components/context/TransactionContext";
+import Web3Provider from "./components/context/Web3Context";
 
 const App = () => {
   return (
-    <TransactionProvider>
-      <div style={{ backgroundColor: " #f7f0f6", height: "100vh" }}>
-        <Navbar />
-        <Home />
-      </div>
-    </TransactionProvider>
+    <div style={{ backgroundColor: " #f7f0f6", height: "100vh" }}>
+      <Navbar />
+      <Home />
+    </div>
   );
 };
 
